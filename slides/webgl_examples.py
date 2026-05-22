90af112368555d331ae9217822e446b61c9d7760
haxybaxy <zaidsaheb4@gmail.com>
2026-05-22 13:22:54 +0200

    add final render

--- slides/webgl_examples.py
+++ slides/webgl_examples.py
@@ -4,13 +4,13 @@ from manim_slides import Slide
 
 class WebGLExamples(Slide):
     def construct(self):
-        logo = SVGMobject("assets/WebGL_Logo.svg").scale_to_fit_height(2.2)
+        logo = SVGMobject("assets/WebGL_Logo.svg").scale_to_fit_height(1.6)
 
-        shot1 = ImageMobject("assets/webglbad1.png").scale_to_fit_height(2.2)
-        shot2 = ImageMobject("assets/webglbad2.png").scale_to_fit_height(2.2)
+        shot1 = ImageMobject("assets/webglbad1.png").scale_to_fit_height(4.0)
+        shot2 = ImageMobject("assets/webglbad2.png").scale_to_fit_height(4.0)
         shots = Group(shot1, shot2).arrange(RIGHT, buff=0.6)
 
-        content = Group(logo, shots).arrange(DOWN, buff=0.5)
+        content = Group(logo, shots).arrange(DOWN, buff=0.4)
         content.move_to(ORIGIN)
 
         self.add(content)
